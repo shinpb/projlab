3bbe387c928dc90f5d68b297fc54e73303ce026b
levboy1 <77939189+levboy1@users.noreply.github.com>
2021-03-27 19:18:49 +0100

    Add files via upload

--- src/Ufo.java
+++ src/Ufo.java
@@ -25,23 +25,29 @@ public class Ufo extends Entity {
 
 	@Override
 	public void solarStormEffect() {
-		
+		Logger.call("Ufo.solarStormEffect()","");
+		Logger.ret("");
 	}
 
 	@Override
 	public void explosionEffect() {
-		
+		Logger.call("Ufo.explosionEffect()","");
+		Logger.ret("");
 	}
 	
 	public void drill() {
-		
+		Logger.call("Ufo.drill()","");
+		Logger.ret("");
 	}
 	
 	public void die() {
-		
+		Logger.call("Ufo.die()","");
+		Logger.ret("");
 	}
 	
 	public void mine() {
+		Logger.call("Ufo.mine()","");
 		position.mineCore();
+		Logger.ret("");
 	}
 }
