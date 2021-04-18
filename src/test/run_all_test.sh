b435cd18deaed3e93518618f65ee4963f879f950
Balint Panczel <panbalint@gmail.com>
2021-04-18 18:56:48 +0200

    test inputs, test runner sh

---
+++ src/test/run_all_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# pls run from /src: ./test/run_all_test.sh
+outd="$(dirname ${0})/out/$(date +%Y-%m-%d_%H-%M-%S)"
+mkdir -p "${outd}"
+javac *.java
+for f in $(cat "$(dirname ${0})/test_names.txt")
+do
+	java Proto < "$(dirname ${0})/in/${f}.in.txt" > "${outd}/${f}.out.txt"
+done
